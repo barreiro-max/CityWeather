7d1ff03b2423a8ea2d5d5e95de50187ad0ec533f
Max <barreiro.fontela.max@gmail.com>
2026-03-13 12:08:15 +0200

    Initial Commit

---
+++ CityWeather/CityWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  CityWeatherApp.swift
+//  CityWeather
+//
+//  Created by MaxAdmin on 13.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct CityWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
