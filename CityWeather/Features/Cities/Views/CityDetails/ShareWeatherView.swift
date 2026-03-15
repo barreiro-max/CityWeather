77ca043c16df3a99f16a5fa5561995640e364449
Max <barreiro.fontela.max@gmail.com>
2026-03-15 19:27:42 +0200

    feat(weather): add ShareWeatherView
    Introduce ShareWeatherView SwiftUI component to display weather information for sharing purposes.

---
+++ CityWeather/Features/Cities/Views/CityDetails/ShareWeatherView.swift
@@ -0,0 +1,53 @@
+import SwiftUI
+
+struct ShareWeatherView: View {
+    @Environment(\.dismiss) private var dismiss
+    let city: City
+    let weather: Weather
+    
+    private var sharedText: String {
+        "Погода в місті \(city.name): \(Int(weather.temperature))°C, \(weather.description.localizedCapitalized)"
+    }
+    
+    @State private var isCopy: Bool = false
+    var body: some View {
+        VStack(spacing: 24) {
+            sharedTextLabel
+            VStack(spacing: 16) {
+                copyButton
+                closeButton
+            }
+            .buttonStyle(.borderedProminent)
+        }
+        .padding()
+    }
+}
+
+extension ShareWeatherView {
+    private var sharedTextLabel: some View {
+        Text(sharedText)
+            .padding()
+            .background(.ultraThinMaterial)
+            .foregroundStyle(isCopy ? .gray : .black)
+            .animation(.easeInOut(duration: 0.2), value: isCopy)
+            .onTapGesture {
+                isCopy.toggle()
+                UIPasteboard.general.string = sharedText
+            }
+            .cornerRadius(8)
+    }
+    
+    private var copyButton: some View {
+        Button {
+            UIPasteboard.general.string = sharedText
+        } label: {
+            Label("Скопіювати", systemImage: "doc.on.doc")
+        }
+    }
+    
+    private var closeButton: some View {
+        Button("Закрити", role: .destructive) {
+            dismiss()
+        }
+    }
+}
