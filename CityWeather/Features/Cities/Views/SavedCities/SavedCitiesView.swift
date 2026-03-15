05cc3f38caef63d2e086244b097ae5af52d13b23
Max <barreiro.fontela.max@gmail.com>
2026-03-15 19:35:55 +0200

    feat(cities): add SavedCitiesView
    Introduce SavedCitiesView SwiftUI screen for displaying the list of saved cities.

---
+++ CityWeather/Features/Cities/Views/SavedCities/SavedCitiesView.swift
@@ -0,0 +1,49 @@
+import SwiftUI
+
+struct SavedCitiesView: View {
+    @Environment(SavedCitiesRouter.self) var router
+    @Environment(CityViewModel.self) var viewModel
+    
+    var body: some View {
+        @Bindable var router = router
+        
+        NavigationStack(path: $router.savedCities) {
+            if viewModel.savedCities.isEmpty {
+                emptySavedCitiesList
+                
+            } else {
+                savedCitiesList
+                    .cityNavigation()
+                    .navigationTitle("Обрані міста")
+            }
+        }
+    }
+    
+    private var emptySavedCitiesList: some View {
+        ContentUnavailableView(
+            "Збережених міст не знайдено",
+            systemImage: "magnifyingglass"
+        )
+    }
+    
+    private var savedCitiesList: some View {
+        List(viewModel.savedCities, id: \.name) { city in
+            Button {
+                router.pushSavedCity(city)
+            } label: {
+                HStack(spacing: 16) {
+                    Text(city.name)
+                        .foregroundStyle(.black)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    SavedCitiesView()
+        .environment(WeatherRouter())
+        .environment(
+            CityViewModel(savedCities: [.chernivtsi, .ivanoFrankivsk, .lviv])
+        )
+}
