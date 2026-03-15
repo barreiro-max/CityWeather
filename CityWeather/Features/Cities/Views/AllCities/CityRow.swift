3a7b33266919e3eff81934d8dabff7165ac6fe49
Max <barreiro.fontela.max@gmail.com>
2026-03-15 19:20:37 +0200

    feat(cities): add CityRow view
    Introduce CityRow SwiftUI component to display individual city information in a list.

---
+++ CityWeather/Features/Cities/Views/AllCities/CityRow.swift
@@ -0,0 +1,22 @@
+import SwiftUI
+
+struct CityRow: View {
+    @Environment(CitiesRouter.self) private var router
+    let city: City
+    
+    var body: some View {
+        Button {
+            router.pushCity(city)
+        } label: {
+            HStack(spacing: 16) {
+                Text(city.name)
+                    .foregroundStyle(.black)
+            }
+        }
+    }
+}
+
+#Preview {
+    CityRow(city: .ivanoFrankivsk)
+        .environment(CitiesRouter())
+}
