6e66b2f377f68668bac51296d2edda64f161d4bf
Max <barreiro.fontela.max@gmail.com>
2026-03-15 18:38:08 +0200

    feat(cities): add City enum
    Add type-safe City enum with CaseIterable support for use in city lists and navigation.

---
+++ CityWeather/Features/Cities/Models/City.swift
@@ -0,0 +1,19 @@
+enum City: String, CaseIterable, Hashable {
+    case kyiv = "Київ"
+    case lviv = "Львів"
+    case odesa = "Одеса"
+    case kharkiv = "Харків"
+    case dnipro = "Дніпро"
+    case uzhhorod = "Ужгород"
+    case zaporizhzhia = "Запоріжжя"
+    case vinnytsia = "Вінниця"
+    case ivanoFrankivsk = "Івано-Франківськ"
+    case ternopil = "Тернопіль"
+    case chernivtsi = "Чернівці"
+    case rivne = "Рівне"
+    case kremenchuk = "Кременчук"
+    
+    var name: String {
+        rawValue
+    }
+}
