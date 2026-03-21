0a326af7b36a78974ce912d2277e7911b3767d2f
Barreiro Fontela Max <barreiro.fontela.max@gmail.com>
2026-03-22 00:49:48 +0200

    feat(cities): add onMove for SavedCitiesView list (#2)
    Implemented onMove for SavedCitiesView list to enable reordering of saved cities.

--- CityWeather/Features/Cities/ViewModels/CityViewModel+Actions.swift
+++ CityWeather/Features/Cities/ViewModels/CityViewModel+Actions.swift
@@ -12,4 +12,14 @@ extension CityViewModel {
     func delete(at offsets: IndexSet) {
         savedCities.remove(atOffsets: offsets)
     }
+    
+    func moveCities(from source: IndexSet, to destination: Int) {
+        guard !source.isEmpty,
+              destination >= 0,
+              destination <= savedCities.count else {
+            return
+        }
+        
+        savedCities.move(fromOffsets: source, toOffset: destination)
+    }
 }
